0bf5aa6212508cb4e46e75ad9d325d5a06ee2ef0
Dan Munro <dan@danmunro.com>
2012-09-12 21:00:26 -0700

    Cleaning up the heartbeat class, getting rid of magic numbers

--- mudpy/heartbeat.py
+++ mudpy/heartbeat.py
@@ -3,6 +3,11 @@ import random, time
 class Heartbeat:
 	instance = None
 
+	TICK_LOWBOUND_SECONDS = 10
+	TICK_HIGHBOUND_SECONDS = 15
+
+	PULSE_SECONDS = 1
+
 	def __init__(self, reactor):
 		self.observers = []
 		self.reactor = reactor
@@ -10,12 +15,12 @@ class Heartbeat:
 	
 	def start(self):
 		i = 0
-		next_tick = 1#random.randint(10, 15)
+		next_tick = self.getTickLength()
 		while(1):
-			time.sleep(1)
-			i += 1
+			time.sleep(Heartbeat.PULSE_SECONDS)
+			i += Heartbeat.PULSE_SECONDS
 			if i == next_tick:
-				next_tick = 1#random.randint(10, 15)
+				next_tick = self.getTickLength()
 				i = 0
 				self.tick()
 	
@@ -32,3 +37,6 @@ class Heartbeat:
 	def tick(self):
 		for i in self.observers:
 			self.reactor.callFromThread(i.tick)
+	
+	def getTickLength(self):
+		return random.randint(Heartbeat.TICK_LOWBOUND_SECONDS, Heartbeat.TICK_HIGHBOUND_SECONDS);
