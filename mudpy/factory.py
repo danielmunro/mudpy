b68b3c91d4c65f28d4ac20ec6b135b64c49b2d91
Dan Munro <dan@danmunro.com>
2012-09-15 07:35:14 -0700

    Factory can now return multiple new instances in one call

--- mudpy/factory.py
+++ mudpy/factory.py
@@ -6,9 +6,11 @@ from command import *
 class Factory:
 	@staticmethod
 	def new(**kwargs):
+		lookups = []
 		for _type, _class in kwargs.iteritems():
 			lookup = startsWith(_class, globals()[_type].__subclasses__());
 			if lookup:
-				return lookup()
+				lookups.append(lookup())
 			else:
 				raise NameError("Factory cannot create a new instance of: "+_type+"."+_class)
+		return lookups if len(lookups) > 1 else lookups[0]
