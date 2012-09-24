1cea5277e0115ba3128eec0ff3d285d9217db604
Dan Munro <dan@danmunro.com>
2012-09-24 11:02:22 -0700

    Adding support for more than one type of event Now there are pulses in addition to ticks, which will handle things like fighting rounds.

--- mudpy/heartbeat.py
+++ mudpy/heartbeat.py
@@ -9,7 +9,7 @@ class Heartbeat:
 	PULSE_SECONDS = 1
 
 	def __init__(self, reactor):
-		self.observers = []
+		self.observers = dict((event, []) for event in ['tick', 'pulse'])
 		self.reactor = reactor
 		Heartbeat.instance = self
 	
@@ -19,24 +19,25 @@ class Heartbeat:
 		while(1):
 			time.sleep(Heartbeat.PULSE_SECONDS)
 			i += Heartbeat.PULSE_SECONDS
+			self.dispatch('pulse')
 			if i > next_tick:
 				next_tick = self.getTickLength()
+				self.dispatch('tick')
 				i = 0
-				self.tick()
 	
-	def attach(self, observer):
-		if not observer in self.observers:
-			self.observers.append(observer)
+	def attach(self, event, observer):
+		if not observer in self.observers[event]:
+			self.observers[event].append(observer)
 	
-	def detach(self, observer):
+	def detach(self, event, observer):
 		try:
-			self.observers.remove(observer)
+			self.observers[event].remove(observer)
 		except ValueError:
 			pass
-	
-	def tick(self):
-		for i in self.observers:
-			self.reactor.callFromThread(i.tick)
+
+	def dispatch(self, event):
+		for i in self.observers[event]:
+			self.reactor.callFromThread(getattr(i, event))
 	
 	def getTickLength(self):
 		return random.randint(Heartbeat.TICK_LOWBOUND_SECONDS, Heartbeat.TICK_HIGHBOUND_SECONDS);
