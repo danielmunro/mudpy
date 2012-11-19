7f2b921553db50bcc080b79aff581cd92e1869b5
Dan Munro <dan@danmunro.com>
2012-11-19 06:44:36 -0800

    simplify db class

--- mudpy/db.py
+++ mudpy/db.py
@@ -2,7 +2,4 @@ import redis
 
 class Db:
 	def __init__(self):
-		self.r = redis.Redis(host='localhost', port=6379, db=10)
-
-	def getConnection(self):
-		return self.r
+		self.conn = redis.Redis(host='localhost', port=6379, db=10)
