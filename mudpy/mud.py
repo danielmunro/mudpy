029ac4a394bec2ea501d6210b67c94a9faecf019
Dan Munro <dan@danmunro.com>
2013-01-20 19:04:50 -0800

    Making parser its own submodule in order to clean it up and hopefully make it more useful

--- mudpy/mud.py
+++ mudpy/mud.py
@@ -3,10 +3,10 @@ from twisted.internet import reactor
 
 from client import ClientFactory
 from heartbeat import Heartbeat
-from parser import Parser
+from parser.parser import Parser
 
 heartbeat = Heartbeat(reactor)
-Parser("areas")
+Parser("areas").initializeRooms()
 
 endpoint = TCP4ServerEndpoint(reactor, 9000)
 endpoint.listen(ClientFactory(heartbeat))
