63975ec71b3beef195a27066d5e2752b36884998
Dan Munro <dan@danmunro.com>
2013-04-13 18:57:17 -0700

    minor coding style tweak

--- mudpy/observer.py
+++ mudpy/observer.py
@@ -12,8 +12,7 @@ class Observer(object):
 	def detach(self, event, fn):
 		try:
 			self.observers[event].remove(fn)
-		except (ValueError, KeyError):
-			pass
+		except (ValueError, KeyError): pass
 	
 	def detachAll(self, *args):
 		self.observers.clear()
