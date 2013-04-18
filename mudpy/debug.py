5f9dca17137b4b3685fac2601537348610e9e6bf
Dan Munro <dan@danmunro.com>
2013-04-18 06:40:09 -0700

    don't open/close the file pointer every log write

--- mudpy/debug.py
+++ mudpy/debug.py
@@ -1,12 +1,17 @@
 import time
 
-def log(message, status = "info"):
-	# write the debug.log file
-	fp = open('debug.log', 'a')
-	fp.write('['+time.strftime('%Y-%m-%d %H:%M:%S')+' '+str(status)+'] '+str(message)+'\n')
-	fp.close()
+fp = open('debug.log', 'a')
+fp.write('\nnew log started\n')
 
-	# better error visibility
+def log(message, status = "info"):
+
+	global fp
+
+	# write the debug.log file
+	fp.write('['+time.strftime('%Y-%m-%d %H:%M:%S')+' '+str(status)+'] '+str(message)+'\n')
+	fp.flush()
+
+	# better error visibility and testing
 	if status == 'error':
 		raise DebugException(message)
 
