75215bfe5178ddb9727d13e0669c04871b1386a7
Dan Munro <dan@danmunro.com>
2013-04-24 20:54:29 -0700

    Changes to affect from pylint

--- mudpy/affect.py
+++ mudpy/affect.py
@@ -1,9 +1,15 @@
+"""Affect has a set of attributes and an optional timeout. These are intended
+to be used to apply attribute changes to an actor or item.
+
+"""
+
 from attributes import Attributes
 from observer import Observer
 from reporter import Reporter
 import debug, heartbeat
 
 class Affect(Observer, Reporter):
+	"""Give an actor or item an affect."""
 
 	def __init__(self):
 		self.name = "an affect"
@@ -13,24 +19,36 @@ class Affect(Observer, Reporter):
 		super(Affect, self).__init__()
 	
 	def start(self, receiver):
+		"""Apply the affect to a receiver."""
+
 		try:
 			self.attach('start', receiver.startAffect)
 			self.attach('end', receiver.endAffect)
 		except AttributeError:
-			debug.log(str(receiver)+" does not have startAffect() and/or endAffect() defined", "error")
+			debug.log(str(receiver)+
+				" does not have startAffect() and/or endAffect() defined",
+				"error")
 		heartbeat.instance.attach('tick', self.tick)
-		self.setAttributesFromReceiver(receiver)
+		self.set_attributes_from_receiver(receiver)
 		self.dispatch(start = self)
 	
-	def setAttributesFromReceiver(self, receiver):
+	def set_attributes_from_receiver(self, receiver):
+		"""Calculate modifiers that are percentages of an attribute of the
+		receiver.
+		
+		"""
+
 		# for any modifiers that are percents, we need to 
 		# get the percent of the receiver's attribute
 		for attr in vars(self.attributes):
 			modifier = getattr(self.attributes, attr)
 			if modifier > 0 and modifier < 1:
-				setattr(self.attributes, attr, receiver.getAttribute(attr) * modifier)
+				setattr(self.attributes, attr, receiver.getAttribute(attr)
+						* modifier)
 	
 	def tick(self):
+		"""Tick listener, count down and remove affect."""
+
 		self.timeout -= 1
 		if self.timeout < 0:
 			heartbeat.instance.detach('tick', self.tick)
