351e5e6bb9e2387a8280f6dfa80a2d702d03bda7
Dan Munro <dan@danmunro.com>
2013-04-27 21:38:48 -0700

    fixing up debug module so pylint is happy

--- mudpy/debug.py
+++ mudpy/debug.py
@@ -1,14 +1,28 @@
+"""Basic logger. Supports three levels of logging:
+
+info - informational, messages go straight to the log file for tailing or
+review later.
+
+notice - probably not a good thing, but not fatal. Will output a message to
+stdout as well as the log file.
+
+error - this is an unrecoverable error. The message will be outputted to stdout
+as well as the log file. Additionally, execution of the mud will come to a
+halt.
+
+"""
+
 import time
 
 fp = open('debug.log', 'a')
 fp.write('\nnew log started\n')
 
 def log(message, status = "info"):
-
-    global fp
+    """Log a message with the given status level."""
 
     # write the debug.log file
-    fp.write('['+time.strftime('%Y-%m-%d %H:%M:%S')+' '+str(status)+'] '+str(message)+'\n')
+    fp.write('['+time.strftime('%Y-%m-%d %H:%M:%S')+' '+str(status)+'] '+ \
+                                                         str(message)+'\n')
     fp.flush()
 
     # better error visibility and testing
@@ -17,4 +31,10 @@ def log(message, status = "info"):
     elif status == 'notice':
         print message
 
-class DebugException(Exception): pass
+class DebugException(Exception):
+    """Raised when an error message is passed to the debugger. Error messages
+    do not kill execution (ie sys.exit()) for easier testability.
+
+    """
+
+    pass
