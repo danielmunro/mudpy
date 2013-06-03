2ae2c54ef2b8bd55c1aa95747c18419a1200a067
Dan Munro <dan@danmunro.com>
2013-06-02 17:35:28 -0700

    Formatting

--- main.py
+++ main.py
@@ -41,5 +41,5 @@ actor.__ACTOR_CONFIG__ = factory.new(actor.Config(), "main")
 
 server.__instance__.config = factory.new(server.Config(), __mud__name__)
 calendar.__instance__.config = factory.new(calendar.Config(), "main")
-server.__instance__.heartbeat.attach('tick', calendar.__instance__.tick)
+server.__instance__.heartbeat.attach("tick", calendar.__instance__.tick)
 server.__instance__.start_listening(client.ClientFactory())
