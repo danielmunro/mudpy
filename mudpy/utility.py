bf544adb3d49727688c73244f4db446cf9245c8a
Dan Munro <dan@danmunro.com>
2013-10-15 13:22:37 +0000

    incremental commit on updating utility module

--- mudpy/utility.py
+++ mudpy/utility.py
@@ -3,24 +3,12 @@ collections of objects that have a name attribute.
 
 """
 
-import inspect
-
 def match_partial(needle, *haystack):
     """Calls checkMatch with the 'find' function, finds any matches in the
     collection names.
 
     """
 
-    return check_match(needle, 'find', haystack)
+    n = str(needle)
 
-def check_match(needle, function, haystack):
-    """checkMatch maps a string function to the name properties of a collection
-    of objects and return any matches.
-
-    """
-
-    for h in haystack:
-        for i in h:
-            att = getattr(getattr(i, 'name').lower(), function)(needle)
-            if not att is False and (att > -1 or att is True):
-                return i() if inspect.isclass(i) else i
+    return next((i for i in haystack if str(i).lower().find(n))
