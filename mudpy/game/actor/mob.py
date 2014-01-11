9b549d689ff8d0d96d39de11cbc2059ad2930f4e
Dan Munro <dan.munro@bigfishgames.com>
2014-01-11 15:31:43 -0800

    cleanup, python2=>3, using new command func

--- mudpy/game/actor/mob.py
+++ mudpy/game/actor/mob.py
@@ -1,7 +1,7 @@
 """Mobs are creatures in the game."""
-from . import actor, disposition
+from ..actor import actor
 from .. import command, room
-import random, __main__
+import random
 
 class Mob(actor.Actor):
     """NPCs of the game, mobs are the inhabitants of the mud world."""
@@ -19,10 +19,10 @@ class Mob(actor.Actor):
         self.auto_flee = False
         self.start_room = None
         self.aggressive = False
-        super(Mob, self).__init__()
-    
+        super().__init__()
+
     def tick(self, _event):
-        super(Mob, self).tick()
+        super().tick()
         if self.movement:
             self._decrement_movement_timer()
         if self.aggressive:
@@ -35,7 +35,7 @@ class Mob(actor.Actor):
         if self.aggressive and actor.last_command.action == "move" and self.get_room().get_actor(actor):
             self.set_target(actor)
             event.handle()
-    
+
     def _decrement_movement_timer(self):
         """Counts down to 0, at which point the mob will attempt to move from
         their current room to a new one. They cannot move to new areas however.
@@ -44,28 +44,28 @@ class Mob(actor.Actor):
 
         self.movement_timer -= 1
         if self.movement_timer < 0:
-            direction = random.choice([direction for direction, _room in 
-                self.get_room().directions.items() if _room and 
-                _room.area == self.get_room().area])
-            command.move(self, direction)
+            direction = random.choice([direction for direction, _room in
+                self.get_room().directions.items() if _room and
+                room.get(_room).area == self.get_room().area])
+            command.do(self, direction)
             self.movement_timer = self.movement
-    
+
     def _normalize_stats(self, _event = None, _args = None):
         if self.curhp < 0:
             self._die()
         super(Mob, self)._normalize_stats()
-    
+
     def _die(self):
         super(Mob, self)._die()
         self.get_room().move_actor(self)
         room.get(room.__config__['purgatory']).arriving(self)
-        __main__.__mudpy__.on('tick', self._respawn)
+        self.publisher.on('tick', self._respawn)
 
     def _respawn(self):
         self.respawn_timer -= 1
         if self.respawn_timer < 0:
-            __main__.__mudpy__.off('tick', self._respawn)
-            self.disposition = disposition.__standing__
+            command.do(self, "wake")
+            self.publisher.off('tick', self._respawn)
             self.curhp = self.get_attribute('hp')
             self.curmana = self.get_attribute('mana')
             self.curmovement = self.get_attribute('movement')
