0617e1236587856ee15030b4972d5d04d38f7de8
Dan Munro <dan.munro@bigfishgames.com>
2014-01-11 15:32:23 -0800

    more generous regen

--- mudpy/game/actor/disposition.py
+++ mudpy/game/actor/disposition.py
@@ -8,8 +8,8 @@ __incapacitated__ = 'incapacitated'
 
 def get_regen_modifier(disposition):
     if disposition == __standing__ or disposition == __incapacitated__:
-        return random.uniform(0.03, 0.08)
+        return random.uniform(0.07, 0.15)
     elif disposition == __laying__ or disposition == __sitting__:
-        return random.uniform(0.09, 0.15)
-    elif diposition == __sleeping__:
-        return random.uniform(0.16, 0.3)
+        return random.uniform(0.16, 0.24)
+    elif disposition == __sleeping__:
+        return random.uniform(0.25, 0.4)
